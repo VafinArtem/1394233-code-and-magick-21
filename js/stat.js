998fcc4a7fe0b63d4a640045cac4bc394491a1cf
Vafin Artem <a.vafin85@gmail.com>
2020-09-19 12:07:18 +0500

    Добавил параметр times в renderStatistics добавил функцию getMaxElement настроил высоту бара в зависимотси от времени прохождения

--- js/stat.js
+++ js/stat.js
@@ -19,7 +19,19 @@ const textField = function (ctx, description, x, y) {
   ctx.fillText(description, x, y);
 };
 
-window.renderStatistics = function (ctx, players) {
+let getMaxElement = function (arr) {
+  let maxElement = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > maxElement) {
+      maxElement = arr[i];
+    }
+  }
+
+  return maxElement;
+};
+
+window.renderStatistics = function (ctx, players, times) {
   renderCloud(ctx, "rgba(0, 0, 0, 0.7)", 110, 20);
   renderCloud(ctx, "#fff", 100, 10);
 
@@ -29,13 +41,15 @@ window.renderStatistics = function (ctx, players) {
   textField(ctx, "Ура вы победили!", 120, 30);
   textField(ctx, "Список результатов:", 120, 50);
 
+  let maxTime = getMaxElement(times);
+
   for (let i = 0; i < players.length; i++) {
     textField(ctx, players[i], CLOUD_Y + (GAP + BAR_WIDTH) * i, CLOUD_HEIGHT - FONT_GAP);
     ctx.save();
     ctx.translate(0, CLOUD_HEIGHT);
     ctx.rotate(-Math.PI / 2);
     ctx.fillStyle = "rgba(255, 0, 0, 1)";
-    ctx.fillRect(CLOUD_X, CLOUD_Y + (GAP + BAR_WIDTH) * i, barHeight, BAR_WIDTH);
+    ctx.fillRect(CLOUD_X, CLOUD_Y + (GAP + BAR_WIDTH) * i, (barHeight * times[i]) / maxTime, BAR_WIDTH);
     ctx.restore();
   }
 };
