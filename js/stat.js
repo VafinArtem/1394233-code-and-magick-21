2b1bc3f20667c68e0e317f32d3ac85a3b31b4e4a
Vafin Artem <a.vafin85@gmail.com>
2020-09-21 21:43:29 +0500

    правка в соответсвии с замечаниями наставника и eslint

--- js/stat.js
+++ js/stat.js
@@ -1,19 +1,21 @@
 "use strict";
 
-const CLOUD_WIDTH = 420;
-const CLOUD_HEIGHT = 270;
-const CLOUD_X = 25;
-const CLOUD_Y = 140;
-const GAP = 50;
+const cloud = {
+  WIDTH: 420,
+  HEIGHT: 270,
+  X: 25,
+  Y: 140
+};
+const COLUMN_GAP = 50;
 const FONT_GAP = 20;
 const TEXT_HEIGHT = 10;
 const BAR_WIDTH = 40;
-const barHeight = CLOUD_HEIGHT - GAP - TEXT_HEIGHT - GAP;
-const PLAYER_COLOR = "rgba(255, 0, 0, 1)";
+const barHeight = cloud.HEIGHT - COLUMN_GAP - TEXT_HEIGHT - COLUMN_GAP;
+const PLAYER_COLOR = `rgba(255, 0, 0, 1)`;
 
 const renderCloud = function (ctx, color, x, y) {
   ctx.fillStyle = color;
-  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+  ctx.fillRect(x, y, cloud.WIDTH, cloud.HEIGHT);
 };
 
 const textField = function (ctx, description, x, y) {
@@ -21,29 +23,25 @@ const textField = function (ctx, description, x, y) {
 };
 
 window.renderStatistics = function (ctx, players, times) {
-  renderCloud(ctx, "rgba(0, 0, 0, 0.7)", 110, 20);
-  renderCloud(ctx, "#fff", 100, 10);
+  renderCloud(ctx, `rgba(0, 0, 0, 0.7)`, 110, 20);
+  renderCloud(ctx, `#fff`, 100, 10);
 
-  ctx.fillStyle = "#000";
-  ctx.font = "16px PT Mono";
-  ctx.textBaseline = "hanging";
-  textField(ctx, "Ура вы победили!", 120, 30);
-  textField(ctx, "Список результатов:", 120, 50);
+  ctx.fillStyle = `#000`;
+  ctx.font = `16px PT Mono`;
+  ctx.textBaseline = `hanging`;
+  textField(ctx, `Ура вы победили!`, 120, 30);
+  textField(ctx, `Список результатов:`, 120, 50);
 
   const maxTime = Math.max(...times);
 
   for (let i = 0; i < players.length; i++) {
-    textField(ctx, players[i], CLOUD_Y + (GAP + BAR_WIDTH) * i, CLOUD_HEIGHT - FONT_GAP);
+    textField(ctx, players[i], cloud.Y + (COLUMN_GAP + BAR_WIDTH) * i, cloud.HEIGHT - FONT_GAP);
     ctx.save();
-    ctx.translate(0, CLOUD_HEIGHT);
+    ctx.translate(0, cloud.HEIGHT);
     ctx.rotate(-Math.PI / 2);
-    if (players[i] === "Вы") {
-      ctx.fillStyle = PLAYER_COLOR;
-    } else {
-      ctx.fillStyle = "hsla(" + 240 * (i - 0.5) + "," + (i + 1) * 15 + "%, 50%, 1)";
-    }
-    ctx.fillRect(CLOUD_X, CLOUD_Y + (GAP + BAR_WIDTH) * i, (barHeight * times[i]) / maxTime, BAR_WIDTH);
+    ctx.fillStyle = players[i] === `Вы` ? PLAYER_COLOR : `hsla(240,` + Math.floor(Math.random() * 100) + `%, 50%, 1)`;
+    ctx.fillRect(cloud.X, cloud.Y + (COLUMN_GAP + BAR_WIDTH) * i, (barHeight * times[i]) / maxTime, BAR_WIDTH);
     ctx.restore();
-    textField(ctx, Math.round(times[i]), CLOUD_Y + (GAP + BAR_WIDTH) * i, (CLOUD_HEIGHT - FONT_GAP * 2) - (barHeight * times[i]) / maxTime);
+    textField(ctx, Math.round(times[i]), cloud.Y + (COLUMN_GAP + BAR_WIDTH) * i, (cloud.HEIGHT - FONT_GAP * 2) - (barHeight * times[i]) / maxTime);
   }
 };
